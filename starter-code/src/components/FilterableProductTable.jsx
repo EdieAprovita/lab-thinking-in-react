977432264184790836b246df95f779a1ac95c9d0
Edgar Chavero <edieveg316@gmail.com>
2020-02-25 21:59:01 -0600

    primer componente

---
+++ starter-code/src/components/FilterableProductTable.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class FilterableProductTable extends Component {
+    render() {
+        return(
+            <div className="IronStore">
+                <h1>IronStore</h1>
+            </div>
+        )
+    }
+}
+
+export default FilterableProductTable;
